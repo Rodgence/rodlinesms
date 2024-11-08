46f011badb7fc470d81efbaa03223dc777f6196e
Ally Seif Salim <108072519+Seifally2@users.noreply.github.com>
2024-11-08 21:53:50 +0300

    extension modified

---
+++ includes/footer.php
@@ -0,0 +1,83 @@
+<footer id="footer" class="footer">
+
+<div class="container footer-top">
+  <div class="row gy-4">
+    <div class="col-lg-5 col-md-12 footer-about">
+      <a href="index.html" class="logo d-flex align-items-center">
+        <span class="sitename">RodLine SMS</span>
+      </a>
+      <p>Welcome to bulk sms services, You can automate your business with auro reply sms from us. We offer instant support to make sure you make it. Lets get started today, Life is good</p>
+      <div class="social-links d-flex mt-4">
+        <a href="https://x.com/RodgenceK"><i class="bi bi-twitter-x"></i></a>
+        <a href="https://facebook.com/rodgence"><i class="bi bi-facebook"></i></a>
+        <a href="https://instagram.com/rodline_tech"><i class="bi bi-instagram"></i></a>
+        <a href="https://linkedin.com/in/rodgence"><i class="bi bi-linkedin"></i></a>
+      </div>
+    </div>
+
+    <div class="col-lg-2 col-6 footer-links">
+      <h4>Useful Links</h4>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="#">Register</a></li>
+        <li><a href="https://login.easyapps.co/#!/">Login</a></li>
+        <li><a href="#">Terms of service</a></li>
+        <li><a href="#">Privacy policy</a></li>
+      </ul>
+    </div>
+
+    <div class="col-lg-2 col-6 footer-links">
+      <h4>Our Services</h4>
+      <ul>
+        <li><a href="#">Bulk SMS</a></li>
+        <li><a href="#">SMS APIs</a></li>
+        <li><a href="#">WordPress Plugin</a></li>
+        <!-- <li><a href="#">Marketing</a></li>
+        <li><a href="#">Graphic Design</a></li> -->
+      </ul>
+    </div>
+
+    <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+      <h4>Contact Us</h4>
+      <p>Dar es salaam</p>
+      <p>Tanzania</p>
+      <!-- <p>United States</p> -->
+      <p class="mt-4"><strong>Phone:</strong> <span>+255769500302</span></p>
+      <p><strong>Email:</strong> <span>info@rodlinesms.co.tz</span></p>
+    </div>
+
+  </div>
+</div>
+
+<div class="container copyright text-center mt-4">
+  <p>© <span>Copyright 2024</span> <strong class="px-1 sitename">RodLine SMS</strong> <span>All Rights Reserved</span></p>
+  <div class="credits">
+  
+    Designed by <a href="https://rodline.co.tz">RodLine Technologies
+  </div>
+</div>
+
+</footer>
+
+<!-- Scroll Top -->
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Preloader -->
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+<!-- Main JS File -->
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
