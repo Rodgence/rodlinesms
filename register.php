24b45b71ba8c6443864cffe39aff15256eea9669
Ally Seif Salim <108072519+Seifally2@users.noreply.github.com>
2024-11-08 22:08:06 +0300

    form added

--- register.php
+++ register.php
@@ -5,30 +5,19 @@
 
   <main class="main">
 
-    <!-- Page Title -->
-    <div class="page-title">
-      <div class="container">
-        <h1>Starter Page</h1>
-        <nav class="breadcrumbs">
-          <ol>
-            <li><a href="index.html">Home</a></li>
-            <li class="current">Starter Page</li>
-          </ol>
-        </nav>
-      </div>
-    </div><!-- End Page Title -->
+   
 
     <!-- Starter Section Section -->
     <section id="starter-section" class="starter-section section">
 
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
-        <h2>Starter Section</h2>
-        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+        <h2>Register for Bulk SMS</h2>
+        <p>Register Now to Send Bulk SMS</p>
       </div><!-- End Section Title -->
 
       <div class="container" data-aos="fade-up">
-        <p>Use this page as a starter for your own custom pages.</p>
+       Form
       </div>
 
     </section><!-- /Starter Section Section -->
